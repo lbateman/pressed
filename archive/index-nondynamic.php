9bd0133c6b297786e80b949ad0f0da106a49d0d4
lbateman <lbateman@mcad.edu>
2014-02-02 15:38:19 -0500

    Initial commit

---
+++ archive/index-nondynamic.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pressed and Squeezed</title>
+	<script src="http://use.edgefonts.net/josefin-slab.js"></script>
+	<script src="http://use.edgefonts.net/merriweather.js"></script>
+	<link rel="stylesheet" href="http://localhost/assignment2-2/wp-content/themes/pressed/style.css"/>
+</head>
+	<body>
+		<header>
+			<h1>Pressed and Squeezed</h1>
+			<nav>
+				<ul>
+					<li>Home</li>
+					<li>WordPress</li>
+				</ul>
+			</nav>
+		</header>
+		<section>
+			<img src="http://localhost/assignment2-2/wp-content/themes/pressed/images/hero.jpg" />
+			<section>
+				<p>Pressed and Squeezed is a full-service WordPress shop. We create custom themes and CMSes for our clients.</p>
+				<p>We also make pancakes on the side. (And if you're really lucky, we'll make them with chocolate chips.)</p>
+			</section>
+		</section>
+		<footer>
+			<h1>Contact Us</h1>
+			<p>Pressed and Squeezed</p>
+			<p>2525 Orange Way, Orchard, CA 91104</p>
+			<p>Phone: (800) 978-9789</p>
+			<p>Here is some static HTML.</p>
+		</footer>
+	</body>
+</html>
